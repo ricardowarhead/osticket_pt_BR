5cc61ed46bd4e91fc8c7e2a2c899e085f4f7bb27
Ricardo Toscano <ricardo.toscano@veezor.com>
2012-07-04 09:57:04 -0300

    upload inicial

---
+++ upload/include/client/thankyou.inc.php
@@ -0,0 +1,33 @@
+<?php
+if(!defined('OSTCLIENTINC') || !is_object($ticket)) die('Kwaheri rafiki!'); //Say bye to our friend..
+
+//Please customize the message below to fit your organization speak!
+?>
+<div>
+    <?if($errors['err']) {?>
+        <p align="center" id="errormessage"><?=$errors['err']?></p>
+    <?}elseif($msg) {?>
+        <p align="center" id="infomessage"><?=$msg?></p>
+    <?}elseif($warn) {?>
+        <p id="warnmessage"><?=$warn?></p>
+    <?}?>
+</div>
+<div style="margin:5px 100px 100px 0;">
+    <?=Format::htmlchars($ticket->getName())?>,<br>
+    <p>
+     Obrigado por nos contactar.<br>
+     Uma solicitação ao suporte foi criada e a Veezor retornará em breve.</p>
+          
+    <?if($cfg->autoRespONNewTicket()){ ?>
+    <p>Um e-mail com o número do ticket foi enviado para <b><?=$ticket->getEmail()?></b>.
+        Você vai precisar do número do ticket e seu e-mail para acessar o progresso da solicitação. 
+    </p>
+    <p>
+     Se você quiser enviar comentários ou informações adicionais sobre o mesmo assunto, por favor siga as instruções no e-mail.
+    </p>
+    <?}?>
+    <p>Equipe de suporte :: Veezor Network Intelligence </p>
+</div>
+<?
+unset($_POST); //clear to avoid re-posting on back button??
+?>
