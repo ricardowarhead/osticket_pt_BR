912f8407a60197822c9a4665b3dd5655e145473d
Ricardo Toscano <ricardo.toscano@veezor.com>
2012-07-05 10:43:15 -0300

    tradução das strings "<th width="165">": Allow Attachments, Emailed Attachments, Online Attachments, Staff Response Files, Maximum File Size, Attachment Folder e Accepted File Types.

--- upload/include/staff/attachment.inc.php
+++ upload/include/staff/attachment.inc.php
@@ -17,7 +17,7 @@ $config=($errors && $_POST)?Format::input($_POST):$cfg->getConfig();
                 Before enabling attachments make sure you understand the security settings and issues related to file uploads.</td>
           </tr>
           <tr>
-            <th width="165">Allow Attachments:</th>
+            <th width="165">Permitir anexos:</th>
             <td>
               <input type="checkbox" name="allow_attachments" <?=$config['allow_attachments'] ?'checked':''?>><b>Allow Attachments</b>
                 &nbsp; (<i>Global Setting</i>)
@@ -25,14 +25,14 @@ $config=($errors && $_POST)?Format::input($_POST):$cfg->getConfig();
             </td>
           </tr>
           <tr>
-            <th>Emailed Attachments:</th>
+            <th>Anexos do e-mail:</th>
             <td>
                 <input type="checkbox" name="allow_email_attachments" <?=$config['allow_email_attachments'] ? 'checked':''?> > Accept emailed files
                     &nbsp;<font class="warn">&nbsp;<?=$warn['allow_email_attachments']?></font>
             </td>
           </tr>
          <tr>
-            <th>Online Attachments:</th>
+            <th>Anexos online:</th>
             <td>
                 <input type="checkbox" name="allow_online_attachments" <?=$config['allow_online_attachments'] ?'checked':''?> >
                     Allow online attachments upload<br/>&nbsp;&nbsp;&nbsp;&nbsp;
@@ -42,20 +42,20 @@ $config=($errors && $_POST)?Format::input($_POST):$cfg->getConfig();
             </td>
           </tr>
           <tr>
-            <th>Staff Response Files:</th>
+            <th>Arquivo resposta do atendente:</th>
             <td>
                 <input type="checkbox" name="email_attachments" <?=$config['email_attachments']?'checked':''?> >Email attachments to the user
             </td>
           </tr>
           <tr>
-            <th nowrap>Maximum File Size:</th>
+            <th nowrap>Tamanho máximo do arquivo:</th>
             <td>
               <input type="text" name="max_file_size" value="<?=$config['max_file_size']?>"> <i>bytes</i>
                 <font class="error">&nbsp;<?=$errors['max_file_size']?></font>
             </td>
           </tr>
           <tr>
-            <th>Attachment Folder:</th>
+            <th>Diretório do anexo:</th>
             <td>
                 Web user (e.g apache) must have write access to the folder. &nbsp;<font class="error">&nbsp;<?=$errors['upload_dir']?></font><br>
               <input type="text" size=60 name="upload_dir" value="<?=$config['upload_dir']?>"> 
@@ -65,7 +65,7 @@ $config=($errors && $_POST)?Format::input($_POST):$cfg->getConfig();
             </td>
           </tr>
           <tr>
-            <th valign="top"><br/>Accepted File Types:</th>
+            <th valign="top"><br/>Tipos de arquivos aceitos:</th>
             <td>
                 Enter file extensions allowed separated by a comma. e.g <i>.doc, .pdf, </i> <br>
                 To accept all files enter wildcard <b><i>.*</i></b>&nbsp;&nbsp;i.e dotStar (NOT recommended).
