64ead43311754c29582f12fa1abeae44e46dece2
root <higor.melo@veezor.com>
2012-07-05 10:46:54 -0300

    tradução revisada

--- upload/include/client/footer.inc.php
+++ upload/include/client/footer.inc.php
@@ -1,6 +1,6 @@
  <div style="clear:both"></div> 
  </div>
- <div id="footer">Copyright &copy; osTicket.com. All rights reserved</div>
+ <div id="footer">Copyright &copy; osTicket.com. Todos os direitos reservados</div>
  <div align="center">
     <!--> As a show of support, we ask that you leave powered by osTicket link to help spread the word. Thank you! -->
      <a id="powered_by" href="http://osticket.com"><img src="./images/poweredby.jpg" width="126" height="23" alt="Powered by osTicket"></a></div>
