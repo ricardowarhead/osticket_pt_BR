46d7e4744a8af72bbac3548b8d588846fd02fa6e
Ricardo Toscano <ricardo.toscano@veezor.com>
2012-07-05 10:15:36 -0300

    tradução de strings entre "<td>": Change Password, Current password, New password e Verify password.

--- upload/include/staff/changepasswd.inc.php
+++ upload/include/staff/changepasswd.inc.php
@@ -2,25 +2,25 @@
 if(!defined('OSTSCPINC') || !is_object($thisuser)) die('Kwaheri');
 $rep=Format::htmlchars($rep);
 ?>
-<div class="msg">Change Password</div>
+<div class="msg">Trocar senha</div>
 <table width="100%" border="0" cellspacing=0 cellpadding=2>
     <form action="profile.php" method="post">
     <input type="hidden" name="t" value="passwd">
     <input type="hidden" name="id" value="<?=$thisuser->getId()?>">
     <tr>
-        <td width="120">Current Password:</td>
+        <td width="120">Senha atual:</td>
         <td>
             <input type="password" name="password" AUTOCOMPLETE=OFF value="<?=$rep['password']?>">
             &nbsp;<font class="error">*&nbsp;<?=$errors['password']?></font></td>
     </tr>
     <tr>
-        <td>New Password:</td>
+        <td>Nova senha:</td>
         <td>
             <input type="password" name="npassword" AUTOCOMPLETE=OFF value="<?=$rep['npassword']?>">
             &nbsp;<font class="error">*&nbsp;<?=$errors['npassword']?></font></td>
     </tr>
     <tr>
-        <td>Verify Password:</td>
+        <td>Verificação de senha:</td>
         <td>
             <input type="password" name="vpassword" AUTOCOMPLETE=OFF value="<?=$rep['vpassword']?>">
             &nbsp;<font class="error">*&nbsp;<?=$errors['vpassword']?></font></td>
