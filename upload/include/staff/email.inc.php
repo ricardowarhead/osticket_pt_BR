955e6aef181c3194111f417600967420474b8199
Ricardo Toscano <ricardo.toscano@veezor.com>
2012-07-06 12:50:56 -0300

    tradução de strings nas linhas: 34, 36, 38, 43, 49, 52, 62, 65, 75, 77, 78, 82, 84, 89, 95, 97, 98, 112, 117, 128, 137, 143, 149, 152, 156, (à terminar).

--- upload/include/staff/email.inc.php
+++ upload/include/staff/email.inc.php
@@ -31,25 +31,25 @@ $priorities= db_query('SELECT priority_id,priority_desc FROM '.TICKET_PRIORITY_T
  <input type="hidden" name="email_id" value="<?=$info['email_id']?>">
  <tr><td>
     <table width="100%" border="0" cellspacing=0 cellpadding=2 class="tform">
-        <tr class="header"><td colspan=2>Email Info</td></tr>
+        <tr class="header"><td colspan=2>Informações do e-mail</td></tr>
         <tr class="subheader">
-            <td colspan=2 >Settings are mainly for emailed tickets. For online/web tickets see help topics.</td>
+            <td colspan=2 >As configurações são principalmente para tickets por e-mail. Para tickets on-line/web, consulte os tópicos de ajuda.</td>
         </tr>
-        <tr><th>Email Address</th>
+        <tr><th>Endereço de e-mail</th>
             <td>
                 <input type="text" name="email" size=30 value="<?=$info['email']?>">&nbsp;<font class="error">*&nbsp;<?=$errors['email']?></font>
             </td>
         </tr>
-        <tr><th>Email Name:</th>
+        <tr><th>Nome do e-mail:</th>
             <td>
                 <input type="text" name="name" size=30 value="<?=$info['name']?>">&nbsp;<font class="error">&nbsp;<?=$errors['name']?></font>
                 &nbsp;&nbsp;(<i>Optional email's FROM name.</i>)
             </td>
         </tr>
-        <tr><th>New Ticket Priority</th>
+        <tr><th>Prioridade do novo ticket</th>
             <td>
                 <select name="priority_id">
-                    <option value=0>Select Priority</option>
+                    <option value=0>Selecionar prioridade</option>
                     <?
                     while (list($id,$name) = db_fetch_row($priorities)){
                         $selected = ($info['priority_id']==$id)?'selected':''; ?>
@@ -59,10 +59,10 @@ $priorities= db_query('SELECT priority_id,priority_desc FROM '.TICKET_PRIORITY_T
                 </select>&nbsp;<font class="error">*&nbsp;<?=$errors['priority_id']?></font>
             </td>
         </tr>
-        <tr><th>New Ticket Dept.</th>
+        <tr><th>Departamento do novo ticket</th>
             <td>
                 <select name="dept_id">
-                    <option value=0>Select Department</option>
+                    <option value=0>Selecionar departamento</option>
                     <?
                     while (list($id,$name) = db_fetch_row($depts)){
                         $selected = ($info['dept_id']==$id)?'selected':''; ?>
@@ -72,30 +72,30 @@ $priorities= db_query('SELECT priority_id,priority_desc FROM '.TICKET_PRIORITY_T
                 </select>&nbsp;<font class="error">&nbsp;<?=$errors['dept_id']?></font>&nbsp;
             </td>
         </tr>
-        <tr><th>Auto Response</th>
+        <tr><th>Resposta automática</th>
             <td>
-                <input type="checkbox" name="noautoresp" value=1 <?=$info['noautoresp']? 'checked': ''?> ><b>Disable</b> autoresponse for this email.
-                &nbsp;&nbsp;(<i>Overwrite Dept setting</i>)
+                <input type="checkbox" name="noautoresp" value=1 <?=$info['noautoresp']? 'checked': ''?> ><b>Desabilitada</b> resposta automática para esse e-mail.
+                &nbsp;&nbsp;(<i>Sobrescrever configuração de departamento</i>)
             </td>
         </tr>
         <tr class="subheader">
-            <td colspan=2 ><b>Login info (optional)</b>: Required when IMAP/POP and/or SMTP are enabled.</td>
+            <td colspan=2 ><b>Informação de login (opcional)</b>: Necessária quando IMAP/POP e/ou SMTP são habilitados.</td>
         </tr>
-        <tr><th>Username</th>
+        <tr><th>Nome do usuário (nickname)</th>
             <td><input type="text" name="userid" size=35 value="<?=$info['userid']?>" autocomplete='off' >
                 &nbsp;<font class="error">&nbsp;<?=$errors['userid']?></font>
             </td>
         </tr>
-        <tr><th>Password</th>
+        <tr><th>Senha</th>
             <td>
                <input type="password" name="userpass" size=35 value="<?=$info['userpass']?>" autocomplete='off'>
                 &nbsp;<font class="error">&nbsp;<?=$errors['userpass']?></font>
             </td>
         </tr>
-        <tr class="header"><td colspan=2>Mail Account (Optional)</b></td></tr>
+        <tr class="header"><td colspan=2>Conta de e-mail (opcional)</b></td></tr>
         <tr class="subheader"><td colspan=2>
-             Setting for fetching incoming emails. Mail fetching must be enabled with autocron active or external cron setup.<br>
-            <b>Please be patient, the system will try to login to mail server to validate the entered login info.</b>
+             Configuração para buscar e-mails recebidos. A busca de e-mail deve ser habilitada com o autocron ativo ou configurar cron externamente.<br>
+            <b>Por favor, seja paciente, o sistema vai tentar entrar no servidor de email para validar a informação de entrada de login.</b>
             <font class="error">&nbsp;<?=$errors['mail']?></font></td></tr>
         <tr><th>Status</th>
             <td>
@@ -109,12 +109,12 @@ $priorities= db_query('SELECT priority_id,priority_desc FROM '.TICKET_PRIORITY_T
                 &nbsp;<font class="error">&nbsp;<?=$errors['mail_host']?></font>
             </td>
         </tr>
-        <tr><th>Port</th>
+        <tr><th>Porta</th>
             <td><input type="text" name="mail_port" size=6 value="<?=$info['mail_port']?$info['mail_port']:''?>">
                 &nbsp;<font class="error">&nbsp;<?=$errors['mail_port']?></font>
             </td>
         </tr>
-        <tr><th>Protocol</th>
+        <tr><th>Protocolo</th>
             <td>
                 <select name="mail_protocol">
                     <option value='POP'>Select</option>
@@ -125,7 +125,7 @@ $priorities= db_query('SELECT priority_id,priority_desc FROM '.TICKET_PRIORITY_T
             </td>
         </tr>
 
-        <tr><th>Encryption</th>
+        <tr><th>Criptografia</th>
             <td>
                  <label><input type="radio" name="mail_encryption"  value="NONE"
                     <?=($info['mail_encryption']!='SSL')?'checked':''?> />None</label>
@@ -134,28 +134,28 @@ $priorities= db_query('SELECT priority_id,priority_desc FROM '.TICKET_PRIORITY_T
                 <font class="error">&nbsp;<?=$errors['mail_encryption']?></font>
             </td>
         </tr>
-        <tr><th>Fetch Frequency</th>
+        <tr><th>Frequencia de busca</th>
             <td>
                 <input type="text" name="mail_fetchfreq" size=4 value="<?=$info['mail_fetchfreq']?$info['mail_fetchfreq']:''?>"> Delay intervals in minutes
                 &nbsp;<font class="error">&nbsp;<?=$errors['mail_fetchfreq']?></font>
             </td>
         </tr>
-        <tr><th>Maximum Emails Per Fetch</th>
+        <tr><th>Máximo de e-mails por busca</th>
             <td>
                 <input type="text" name="mail_fetchmax" size=4 value="<?=$info['mail_fetchmax']?$info['mail_fetchmax']:''?>"> Maximum emails to process per fetch.
                 &nbsp;<font class="error">&nbsp;<?=$errors['mail_fetchmax']?></font>
             </td>
         </tr>
-        <tr><th>Delete Messages</th>
+        <tr><th>Apagar mensagens</th>
             <td>
                 <input type="checkbox" name="mail_delete" value=1 <?=$info['mail_delete']? 'checked': ''?> >
-                    Delete fetched message(s) (<i>recommended when using POP</i>)
+                    Delete fetched message(s) (<i>recomendado quando usar POP</i>)
                 &nbsp;<font class="error">&nbsp;<?=$errors['mail_delete']?></font>
             </td>
         </tr>
-        <tr class="header"><td colspan=2>SMTP Settings (Optional)</b></td></tr>
+        <tr class="header"><td colspan=2>Configurações SMTP (opcional)</b></td></tr>
         <tr class="subheader"><td colspan=2>
-             When enabled the <b>email account</b> will use SMTP server instead of internal PHP mail() function for outgoing emails.<br>
+             Quando habilitar o <b>email account</b> vai usar o servidor SMTP ao invés de usar a função interna mail() do PHP para saída de e-mails.<br>
             <b>Please be patient, the system will try to login to SMTP server to validate the entered login info.</b>
                 <font class="error">&nbsp;<?=$errors['smtp']?></font></td></tr>
         <tr><th>Status</th>
