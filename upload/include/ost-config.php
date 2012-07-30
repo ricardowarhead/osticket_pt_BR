8f0ed7a8b1259db7529df7a87a0403027f696f86
Ricardo Toscano <ricardo.toscano@veezor.com>
2012-07-30 10:59:04 -0300

    versão original do arquivo

--- upload/include/ost-config.php
+++ upload/include/ost-config.php
@@ -20,7 +20,7 @@
 if(!strcasecmp(basename($_SERVER['SCRIPT_NAME']),basename(__FILE__)) || !defined('ROOT_PATH')) die('kwaheri rafiki!');
 
 #Install flag
-define('OSTINSTALLED',TRUE);
+define('OSTINSTALLED',FALSE);
 if(OSTINSTALLED!=TRUE){
     if(!file_exists(ROOT_PATH.'setup/install.php')) die('Error: Contact system admin.'); //Something is really wrong!
     //Invoke the installer.
@@ -29,20 +29,19 @@ if(OSTINSTALLED!=TRUE){
 }
 
 # Encrypt/Decrypt secret key - randomly generated during installation.
-define('SECRET_SALT','6829FCA0C667180');
+define('SECRET_SALT','%CONFIG-SIRI');
 
 #Default admin email. Used only on db connection issues and related alerts.
-define('ADMIN_EMAIL','ricardowarhead@gmail.com');
-define('ADMIN_EMAIL','higor.melo@veezor.com');
+define('ADMIN_EMAIL','%ADMIN-EMAIL');
 
 #Mysql Login info
 define('DBTYPE','mysql');
-define('DBHOST','localhost'); 
-define('DBNAME','osticket');
-define('DBUSER','root');
-define('DBPASS','veezor');
+define('DBHOST','%CONFIG-DBHOST'); 
+define('DBNAME','%CONFIG-DBNAME');
+define('DBUSER','%CONFIG-DBUSER');
+define('DBPASS','%CONFIG-DBPASS');
 
 #Table prefix
-define('TABLE_PREFIX','ost_');
+define('TABLE_PREFIX','%CONFIG-PREFIX');
 
 ?>
