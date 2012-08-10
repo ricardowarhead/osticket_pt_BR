c87ec3b553e9ca9c7c58a05af79c65087fae85e5
Ricardo Toscano <ricardo.toscano@veezor.com>
2012-08-10 12:33:41 -0300

    Update upload/offline.php
    tradução de strings de informações ao cliente

--- upload/offline.php
+++ upload/offline.php
@@ -23,17 +23,17 @@ if($cfg && !$cfg->isHelpDeskOffline()) {
 ?>
 <html>
 <head>
-<title>Support Ticket System</title>
+<title>Sistema de Suporte por Ticket</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 </head>
 <body bgcolor="#FFFFFF" text="#000000" leftmargin="0" rightmargin="0" topmargin="0">
 <table width="60%" cellpadding="5" cellspacing="0" border="0">
 	<tr<td>
         <p>
-         <h3>Support Ticket System Offline</h3>
+         <h3>Sistema de Suporte por Ticket Offline</h3>
          
-         Thank you for your interest in contacting us.<br>
-         Our helpdesk is offline at the moment, please check back at a later time.
+         Obrigado por seu interesse em nos contatar.<br>
+         Nosso helpdesk está offline no momento, por favor, verifique novamente em um momento posterior.
         </p>
     </td></tr>
 </table>
