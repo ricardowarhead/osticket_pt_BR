1f08309d95fdcfeea185729487ea09c34a94a985
Ricardo Toscano <ricardo.toscano@veezor.com>
2012-08-17 10:40:36 -0300

    atualização e tradução de strings

--- upload/include/staff/attachment.inc.php
+++ upload/include/staff/attachment.inc.php
@@ -1,5 +1,5 @@
 <?php
-if(!defined('OSTADMININC') || !$thisuser->isadmin()) die('Access Denied');
+if(!defined('OSTADMININC') || !$thisuser->isadmin()) die('Acesso Negado');
 //Get the config info.
 $config=($errors && $_POST)?Format::input($_POST):$cfg->getConfig();
 ?>
@@ -10,24 +10,24 @@ $config=($errors && $_POST)?Format::input($_POST):$cfg->getConfig();
       <td>
         <table width="100%" border="0" cellspacing=0 cellpadding=2 class="tform">
           <tr class="header">
-            <td colspan=2>&nbsp;Attachments Settings</td>
+            <td colspan=2>&nbsp;Configurações de Anexo</td>
           </tr>
           <tr class="subheader">
             <td colspan=2">
-                Before enabling attachments make sure you understand the security settings and issues related to file uploads.</td>
+                Antes de habilitar anexos certifique-se de compreender as configurações de segurança e questões relacionadas com uploads de arquivos.</td>
           </tr>
           <tr>
             <th width="165">Permitir anexos:</th>
             <td>
-              <input type="checkbox" name="allow_attachments" <?=$config['allow_attachments'] ?'checked':''?>><b>Allow Attachments</b>
-                &nbsp; (<i>Global Setting</i>)
+              <input type="checkbox" name="allow_attachments" <?=$config['allow_attachments'] ?'checked':''?>><b>Permitir Anexos</b>
+                &nbsp; (<i>Configuração Global</i>)
                 &nbsp;<font class="error">&nbsp;<?=$errors['allow_attachments']?></font>
             </td>
           </tr>
           <tr>
             <th>Anexos do e-mail:</th>
             <td>
-                <input type="checkbox" name="allow_email_attachments" <?=$config['allow_email_attachments'] ? 'checked':''?> > Accept emailed files
+                <input type="checkbox" name="allow_email_attachments" <?=$config['allow_email_attachments'] ? 'checked':''?> > Aceitar arquivos por e-mail
                     &nbsp;<font class="warn">&nbsp;<?=$warn['allow_email_attachments']?></font>
             </td>
           </tr>
@@ -35,16 +35,16 @@ $config=($errors && $_POST)?Format::input($_POST):$cfg->getConfig();
             <th>Anexos online:</th>
             <td>
                 <input type="checkbox" name="allow_online_attachments" <?=$config['allow_online_attachments'] ?'checked':''?> >
-                    Allow online attachments upload<br/>&nbsp;&nbsp;&nbsp;&nbsp;
+                    Permitir o upload de anexos on-line<br/>&nbsp;&nbsp;&nbsp;&nbsp;
                 <input type="checkbox" name="allow_online_attachments_onlogin" <?=$config['allow_online_attachments_onlogin'] ?'checked':''?> >
-                    Authenticated users Only. (<i>User must be logged in to upload files </i>)
+                    Somente usuários autenticados. (<i>O usuário deve estar logado para fazer o upload de arquivos </i>)
                     <font class="warn">&nbsp;<?=$warn['allow_online_attachments']?></font>
             </td>
           </tr>
           <tr>
             <th>Arquivo resposta do atendente:</th>
             <td>
-                <input type="checkbox" name="email_attachments" <?=$config['email_attachments']?'checked':''?> >Email attachments to the user
+                <input type="checkbox" name="email_attachments" <?=$config['email_attachments']?'checked':''?> >Anexos para usuário de e-mail
             </td>
           </tr>
           <tr>
@@ -57,7 +57,7 @@ $config=($errors && $_POST)?Format::input($_POST):$cfg->getConfig();
           <tr>
             <th>Diretório do anexo:</th>
             <td>
-                Web user (e.g apache) must have write access to the folder. &nbsp;<font class="error">&nbsp;<?=$errors['upload_dir']?></font><br>
+                Usuário de Web (apache por exemplo) deve ter acesso de gravação (WRITE) para a pasta. &nbsp;<font class="error">&nbsp;<?=$errors['upload_dir']?></font><br>
               <input type="text" size=60 name="upload_dir" value="<?=$config['upload_dir']?>"> 
               <font color=red>
               <?=$attwarn?>
@@ -67,16 +67,16 @@ $config=($errors && $_POST)?Format::input($_POST):$cfg->getConfig();
           <tr>
             <th valign="top"><br/>Tipos de arquivos aceitos:</th>
             <td>
-                Enter file extensions allowed separated by a comma. e.g <i>.doc, .pdf, </i> <br>
-                To accept all files enter wildcard <b><i>.*</i></b>&nbsp;&nbsp;i.e dotStar (NOT recommended).
+                Digite as extensões de arquivo permitidas separadas por uma vírgula. por exemplo <i>.doc, .pdf, </i> <br>
+                Para aceitar todos os arquivos digite asterístico <b><i>.*</i></b>&nbsp;&nbsp;i.e dotStar (NÃO recomendado).
                 <textarea name="allowed_filetypes" cols="21" rows="4" style="width: 65%;" wrap=HARD ><?=$config['allowed_filetypes']?></textarea>
             </td>
           </tr>
         </table>
     </td></tr>
     <tr><td style="padding:10px 0 10px 200px">
-        <input class="button" type="submit" name="submit" value="Save Changes">
-        <input class="button" type="reset" name="reset" value="Reset Changes">
+        <input class="button" type="submit" name="submit" value="Salvar Mudanças">
+        <input class="button" type="reset" name="reset" value="Redefinir Mudanças">
     </td></tr>
   </form>
 </table>
