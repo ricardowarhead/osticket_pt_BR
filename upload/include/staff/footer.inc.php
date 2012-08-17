316d536168b732927c6a3e647b5f197cf338ba31
Higor Melo <higor.melo@veezor.com>
2012-08-17 11:39:10 -0300

    tradução de strings ainda não traduzidas

--- upload/include/staff/footer.inc.php
+++ upload/include/staff/footer.inc.php
@@ -1,7 +1,6 @@
-
 </div> 
 </div>
-<div id="footer">Copyright &copy; 2006-<?=date('Y')?>&nbsp;osTicket.com. &nbsp;All Rights Reserved.</div>
+<div id="footer">Copyright &copy; 2006-<?=date('Y')?>&nbsp;Veezor.com. &nbsp;Todos os Direitos Reservados.</div>
 <?if(is_object($thisuser) && $thisuser->isStaff()) {?>
 <div>
     <!-- Do not remove <img src="autocron.php" alt="" width="1" height="1" border="0" /> or your auto cron will cease to function -->
