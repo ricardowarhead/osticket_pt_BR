910b2ba9e444ef051db2a21ea9618e1d9401d09c
Higor Melo <higor.melo@veezor.com>
2012-08-17 12:07:25 -0300

    tradução de lista de banidos

--- upload/include/staff/banlist.inc.php
+++ upload/include/staff/banlist.inc.php
@@ -1,5 +1,5 @@
 <?php
-if(!defined('OSTADMININC') || !$thisuser->isadmin()) die('Access Denied');
+if(!defined('OSTADMININC') || !$thisuser->isadmin()) die('Acesso Negado');
 
 $select='SELECT * ';
 $from='FROM '.BANLIST_TABLE;
@@ -7,7 +7,7 @@ $where='';
 //make sure the search query is 3 chars min...defaults to no query with warning message
 if($_REQUEST['a']=='search') {
     if(!$_REQUEST['query'] || strlen($_REQUEST['query'])<3) {
-        $errors['err']='Search term must be more than 3 chars';
+        $errors['err']='O termo a pesquisar deve ter mais de 3 caracteres';
     }else{
         //Do the search
         $search=true;
@@ -50,44 +50,44 @@ $query="$select $from $where $order_by LIMIT ".$pageNav->getStart().",".$pageNav
 //echo $query;
 $banlist = db_query($query);
 $showing=db_num_rows($banlist)?$pageNav->showing():'';
-$result_type=($search)?'Search Results':'Banned Email Addresses';
+$result_type=($search)?'Resultados da pesquisa':'Banido endereços de e-mail';
 $negorder=$order=='DESC'?'ASC':'DESC'; //Negate the sorting..
 $showadd=($errors && $_POST['a']=='add')?true:false;
 ?>
 <div id='search' align="left"  style="display:<?=$showadd?'none':'block'?>;">
     <form action="admin.php?t=settings" method="GET" >
-        <input type='hidden' name='t' value='banlist'>
-        <input type='hidden' name='a' value='search'>
-        Query:&nbsp;<input type="text" name="query" value="<?=Format::htmlchars($_REQUEST['query'])?>">
+        <input type='hidden' name='t' value='banidos'>
+        <input type='hidden' name='a' value='pesquisar'>
+        Pesquisar:&nbsp;<input type="text" name="query" value="<?=Format::htmlchars($_REQUEST['query'])?>">
         &nbsp;&nbsp;
-        <input type="submit" name="search" class="button" value="Search">
-        &nbsp;<a href="#" class="msg" onClick="showHide('add','search'); return false;">(Add)</a>
+        <input type="submit" name="search" class="button" value="pesquisar">
+        &nbsp;<a href="#" class="msg" onClick="showHide('add','search'); return false;">(Adicionar)</a>
     </form>
 </div>
 <div id='add' style="display:<?=$showadd?'block':'none'?>;" align="left">
     <form action="admin.php?t=settings" method="POST" >
-    <input type='hidden' name='t' value='banlist'>
-    <input type='hidden' name='a' value='add'>
+    <input type='hidden' name='t' value='banidos'>
+    <input type='hidden' name='a' value='adicinonar'>
     Email:&nbsp;<input type="text" name="email" value="<?=$_POST['email']?>">
     &nbsp;
-    <input type="submit" name="add" class="button" value="Add">
-     &nbsp;<a href="#" class="msg" onClick="showHide('add','search'); return false;">(Search)</a>
+    <input type="submit" name="add" class="button" value="Adicionar">
+     &nbsp;<a href="#" class="msg" onClick="showHide('add','search'); return false;">(Pesquisar)</a>
     </form>
 </div>
 <div class="msg"><?=$result_type?>:&nbsp;<?=$showing?></div>
  <table width="100%" border="0" cellspacing=1 cellpadding=2>
    <form action="admin.php?t=banlist" method="POST" name="banlist" onSubmit="return checkbox_checker(document.forms['banlist'],1,0);">
-   <input type=hidden name='t' value='banlist'>
-   <input type=hidden name='a' value='remove'>
+   <input type=hidden name='t' value='banidos'>
+   <input type=hidden name='a' value='remover'>
    <tr><td>
     <table border="0" cellspacing=0 cellpadding=2 class="dtable" align="center" width="100%">
         <tr>
 	        <th width="7px">&nbsp;</th>
 	        <th width=250>
-                <a href="admin.php?t=banlist&sort=email&order=<?=$negorder?><?=$qstr?>" title="Sort by email <?=$negorder?>">Email</a></th>
+                <a href="admin.php?t=banlist&sort=email&order=<?=$negorder?><?=$qstr?>" title="Ordenar por e-mail <?=$negorder?>">Email</a></th>
 	        <th width=200 >Submitter</th> 
 	        <th width=150 nowrap>
-                <a href="admin.php?t=banlist&sort=date&order=<?=$negorder?><?=$qstr?>" title="Sort By Create Date <?=$negorder?>">Date Added</a></th>
+                <a href="admin.php?t=banlist&sort=date&order=<?=$negorder?><?=$qstr?>" title="Ordenar por Criar Data <?=$negorder?>">Data Adicionada</a></th>
         </tr>
         <?
         $class = 'row1';
@@ -113,7 +113,7 @@ $showadd=($errors && $_POST['a']=='add')?true:false;
             $class = ($class =='row2') ?'row1':'row2';
             } //end of while.
         else: //nothin' found!! ?> 
-            <tr class="<?=$class?>"><td colspan=4><b>Query returned 0 results</b>&nbsp;&nbsp;<a href="admin.php?t=banlist">Index list</a></td></tr>
+            <tr class="<?=$class?>"><td colspan=4><b>Consulta retornou 0 resultados</b>&nbsp;&nbsp;<a href="admin.php?t=banlist">Índice de lista</a></td></tr>
         <?
         endif; ?>
      </table>
@@ -123,18 +123,18 @@ $showadd=($errors && $_POST['a']=='add')?true:false;
      ?>
     <tr>
         <td style="padding-left:20px">
-            Select:&nbsp;
-            <a href="#" onclick="return select_all(document.forms['banlist'],true)">All</a>&nbsp;&nbsp;
-            <a href="#" onclick="return toogle_all(document.forms['banlist'],true)">Toggle</a>&nbsp;&nbsp;
-            <a href="#" onclick="return reset_all(document.forms['banlist'])">None</a>&nbsp;&nbsp;
+            Selecione:&nbsp;
+            <a href="#" onclick="return select_all(document.forms['banlist'],true)">Todos</a>&nbsp;&nbsp;
+            <a href="#" onclick="return toogle_all(document.forms['banlist'],true)">Alternar</a>&nbsp;&nbsp;
+            <a href="#" onclick="return reset_all(document.forms['banlist'])">Nenhum</a>&nbsp;&nbsp;
             &nbsp;page:<?=$pageNav->getPageLinks()?>
             
         </td>
     </tr>
     <tr>
         <td align="center">
-            <input class="button" type="submit" name="delete" value="Remove From List" 
-                     onClick='return confirm("Are you sure you want to REMOVE selected email from ban list?");'>
+            <input class="button" type="submit" name="delete" value="Remover da lista" 
+                     onClick='return confirm("Tem certeza que deseja remover e-mails selecionados da lista de proibição?");'>
         </td>
     </tr>
     <?
