29ebd63ecdd9e875c100e3c7588bc7cc44271a63
Higor Melo <higor.melo@veezor.com>
2012-08-21 11:16:49 -0300

    tradução de strings ainda não traduzidas

--- upload/include/staff/premade.inc.php
+++ upload/include/staff/premade.inc.php
@@ -1,5 +1,5 @@
 <?php
-if(!defined('OSTSCPINC') or !is_object($thisuser) or !$thisuser->canManageKb()) die('Access Denied');
+if(!defined('OSTSCPINC') or !is_object($thisuser) or !$thisuser->canManageKb()) die('Acesso Negado');
 
 //List premade answers.
 $select='SELECT premade.*,dept_name ';
@@ -8,7 +8,7 @@ $from='FROM '.KB_PREMADE_TABLE.' premade LEFT JOIN '.DEPT_TABLE.' USING(dept_id)
 //make sure the search query is 3 chars min...defaults to no query with warning message
 if($_REQUEST['a']=='search') {
     if(!$_REQUEST['query'] || strlen($_REQUEST['query'])<3) {
-        $errors['err']='Search term must be more than 3 chars';
+        $errors['err']='O termo a pesquisar deve ter mais de 3 caracteres';
     }else{
         //fulltext search.
         $search=true;
@@ -50,7 +50,7 @@ $query="$select $from $where $order_by LIMIT ".$pageNav->getStart().",".$pageNav
 //echo $query;
 $replies = db_query($query);
 $showing=db_num_rows($replies)?$pageNav->showing():'';
-$results_type=($search)?'Search Results':'Premade/Canned Replies';
+$results_type=($search)?'Resultados da Pesquisa':'Premade/Canned Replies';
 $negorder=$order=='DESC'?'ASC':'DESC'; //Negate the sorting..
 ?>
 <div>
@@ -64,7 +64,7 @@ $negorder=$order=='DESC'?'ASC':'DESC'; //Negate the sorting..
 </div>
 <div align="left">
     <form action="kb.php" method="GET" >
-    <input type='hidden' name='a' value='search'>
+    <input type='hidden' name='a' value='pesquisar'>
     Procurar por:&nbsp;<input type="text" name="query" value="<?=Format::htmlchars($_REQUEST['query'])?>">
     categoria
     <select name="dept">
@@ -77,7 +77,7 @@ $negorder=$order=='DESC'?'ASC':'DESC'; //Negate the sorting..
            <?}?>
     </select>
     &nbsp;
-    <input type="submit" name="search" class="button" value="GO">
+    <input type="submit" name="search" class="button" value="Avançar">
     </form>
 </div>
 <div class="msg"><?=$result_type?>&nbsp;<?=$showing?></div>
@@ -90,8 +90,8 @@ $negorder=$order=='DESC'?'ASC':'DESC'; //Negate the sorting..
 	        <th width="7px">&nbsp;</th>
 	        <th>
                 <a href="kb.php?sort=title&order=<?=$negorder?><?=$qstr?>" title="Sort By Title <?=$negorder?>">Título da Resposta</a></th>
-            <th width=50>Status</th>
-	        <th width=200>Categoria/Dept</th> 
+            <th width=50>Estado</th>
+	        <th width=200>Categoria/Departamento</th> 
 	        <th width=150 nowrap>
                 <a href="kb.php?sort=updatedate&order=<?=$negorder?><?=$qstr?>" title="Sort By Update Date <?=$negorder?>">Último Updated</a></th>
         </tr>
@@ -114,8 +114,8 @@ $negorder=$order=='DESC'?'ASC':'DESC'; //Negate the sorting..
                   <input type="checkbox" name="canned[]" value="<?=$row['premade_id']?>" <?=$sel?'checked':''?> 
                         onClick="highLight(this.value,this.checked);">
                 <td><a href="kb.php?id=<?=$row['premade_id']?>"><?=Format::htmlchars(Format::truncate($row['title'],60))?></a></td>
-                <td><b><?=$row['isenabled']?'Active':'Disabled'?></b></td>
-                <td><?=$row['dept_name']?Format::htmlchars($row['dept_name']):'All Departments'?></td>
+                <td><b><?=$row['isenabled']?'Ativado':'Desativado'?></b></td>
+                <td><?=$row['dept_name']?Format::htmlchars($row['dept_name']):'Todos os Departamentos'?></td>
                 <td><?=Format::db_datetime($row['updated'])?></td>
             </tr>
             <?
@@ -138,12 +138,12 @@ $negorder=$order=='DESC'?'ASC':'DESC'; //Negate the sorting..
         &nbsp;page:<?=$pageNav->getPageLinks()?>&nbsp;
     </td></td>
     <tr><td align="center">
-            <input class="button" type="submit" name="enable" value="Enable" 
-                onClick='return confirm("Are you sure you want to ENABLE selected entries?");'>
-            <input class="button" type="submit" name="disable" value="Disable" 
-                onClick='return confirm("Are you sure you want to DISABLE selected entries?");'>
-            <input class="button" type="submit" name="delete" value="Delete" 
-                onClick='return confirm("Are you sure you want to DELETE selected entries?");'>
+            <input class="button" type="submit" name="enable" value="Ativar" 
+                onClick='return confirm("Tem certeza de que deseja ATIVAR entradas selecionadas?");'>
+            <input class="button" type="submit" name="disable" value="Desativar" 
+                onClick='return confirm("Tem certeza de que deseja DESATIVAR entradas selecionadas?");'>
+            <input class="button" type="submit" name="delete" value="Apagar" 
+                onClick='return confirm("Tem certeza de que quer APAGAR entradas selecionadas?");'>
     </td></td>
     <?
     endif;
