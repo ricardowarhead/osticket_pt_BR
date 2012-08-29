e135d5b1dc10c7fa0c8fed9425938441b044f430
Ricardo Toscano <ricardo.toscano@veezor.com>
2012-08-29 10:25:01 -0300

    tradução de rótulos
    erros

--- upload/include/class.http.php
+++ upload/include/class.http.php
@@ -19,22 +19,22 @@ class Http {
     function header_code_verbose($code) {
         switch($code):
         case 200: return '200 OK';
-        case 204: return '204 NoContent';
-        case 401: return '401 Unauthorized';
-        case 403: return '403 Forbidden';
-        case 405: return '405 Method Not Allowed';
-        case 416: return '416 Requested Range Not Satisfiable';
-        default:  return '500 Internal Server Error';
+        case 204: return '204 Sem Conteúdo';
+        case 401: return '401 Não Autorizado';
+        case 403: return '403 Proibido';
+        case 405: return '405 Método Não Permitido';
+        case 416: return '416 Intervalo Solicitado Não Satisfatório';
+        default:  return '500 Erro Interno do Servidor';
         endswitch;
     }
     
     function response($code,$content,$contentType='text/html',$charset='UTF-8') {
 		
         header('HTTP/1.1 '.Http::header_code_verbose($code));
-		header('Status: '.Http::header_code_verbose($code)."\r\n");
-		header("Connection: Close\r\n");
-		header("Content-Type: $contentType; charset=$charset\r\n");
-        header('Content-Length: '.strlen($content)."\r\n\r\n");
+		header('Situação: '.Http::header_code_verbose($code)."\r\n");
+		header("Conexão: Fechada\r\n");
+		header("Tipo de conteúdo: $contentType; charset=$charset\r\n");
+        header('Tamanho do conteúdo: '.strlen($content)."\r\n\r\n");
        	print $content;
         exit;
     }
@@ -42,9 +42,9 @@ class Http {
 	function redirect($url,$delay=0,$msg='') {
 
         if(strstr($_SERVER['SERVER_SOFTWARE'], 'IIS')){
-            header("Refresh: $delay; URL=$url");
+            header("Atualização: $delay; URL=$url");
         }else{
-            header("Location: $url");
+            header("Localização: $url");
         }
         exit;
     }
