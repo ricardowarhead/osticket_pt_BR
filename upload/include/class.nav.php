c3c4f6407ae36596e5e73e5e2c05b192ff4fb9d6
Ricardo Toscano <ricardo.toscano@veezor.com>
2012-08-31 10:09:50 -0300

    tradução de legendas e correção de rótulos

--- upload/include/class.nav.php
+++ upload/include/class.nav.php
@@ -27,19 +27,19 @@ class StaffNav {
         $this->ptype=$pagetype;
         $tabs=array();
         if($thisuser->isAdmin() && strcasecmp($pagetype,'admin')==0) {
-            $tabs['dashboard']=array('desc'=>'Painel de Controle','href'=>'admin.php?t=dashboard','title'=>'Admin Dashboard');
-            $tabs['settings']=array('desc'=>'Configurações','href'=>'admin.php?t=settings','title'=>'System Settings');
-            $tabs['emails']=array('desc'=>'E-mails','href'=>'admin.php?t=email','title'=>'Email Settings');
-            $tabs['topics']=array('desc'=>'Tópicos de Ajuda','href'=>'admin.php?t=topics','title'=>'Help Topics');
-            $tabs['staff']=array('desc'=>'Atendente','href'=>'admin.php?t=staff','title'=>'Staff Members');
-            $tabs['depts']=array('desc'=>'Departamentos','href'=>'admin.php?t=depts','title'=>'Departments');
+            $tabs['dashboard']=array('desc'=>'Painel de Controle','href'=>'admin.php?t=dashboard','title'=>'Painel de Controle');
+            $tabs['settings']=array('desc'=>'Configurações','href'=>'admin.php?t=settings','title'=>'Configurações do Sistema');
+            $tabs['emails']=array('desc'=>'E-mails','href'=>'admin.php?t=email','title'=>'Configurações de E-mail');
+            $tabs['topics']=array('desc'=>'Tópicos de Ajuda','href'=>'admin.php?t=topics','title'=>'Tópicos de Ajuda');
+            $tabs['staff']=array('desc'=>'Atendente','href'=>'admin.php?t=staff','title'=>'Membros');
+            $tabs['depts']=array('desc'=>'Departamentos','href'=>'admin.php?t=depts','title'=>'Departamentos');
         }else {
-            $tabs['tickets']=array('desc'=>'Tickets','href'=>'tickets.php','title'=>'Ticket Queue');
+            $tabs['tickets']=array('desc'=>'Tickets','href'=>'tickets.php','title'=>'Fila de Tickets');
             if($thisuser && $thisuser->canManageKb()){
-             $tabs['kbase']=array('desc'=>'Respostas','href'=>'kb.php','title'=>'Knowledge Base: Premade');
+             $tabs['kbase']=array('desc'=>'Respostas','href'=>'kb.php','title'=>'Base de Dados: Respostas');
             }
-            $tabs['directory']=array('desc'=>'Diretório','href'=>'directory.php','title'=>'Staff Directory');
-            $tabs['profile']=array('desc'=>'Minha Conta','href'=>'profile.php','title'=>'My Profile');
+            $tabs['directory']=array('desc'=>'Comitiva','href'=>'directory.php','title'=>'Comissão Diretora');
+            $tabs['profile']=array('desc'=>'Minha Conta','href'=>'profile.php','title'=>'Meu Perfil');
         }
         $this->tabs=$tabs;    
     }
